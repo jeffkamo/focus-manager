4beaf3a210e5bb98e55798b7f4da82af687c7b82
Jeff Kamo <kamojeffrey@gmail.com>
2015-05-01 14:36:02 -0700

    Add reset() test

---
+++ tests/unit/reset.js
@@ -0,0 +1,31 @@
+define([
+    'test-sandbox'
+], function(testSandbox) {
+    var FocusManager;
+    var focusManager;
+    var $;
+
+    describe('FocusManager send', function() {
+        beforeEach(function(done) {
+            var setUpComplete = function(iFrame$, dependencies) {
+                $ = iFrame$;
+
+                FocusManager = dependencies.FocusManager;
+
+                done();
+            };
+
+            testSandbox.setUp('sandbox', setUpComplete);
+        });
+
+        it('should reset the stack back to an empty array', function() {
+            FocusManager.store($('<div></div>'));
+            FocusManager.store($('<div></div>'));
+            FocusManager.store($('<div></div>'));
+
+            FocusManager.reset();
+
+            expect(FocusManager.stack).to.eql([]);
+        });
+    });
+});
